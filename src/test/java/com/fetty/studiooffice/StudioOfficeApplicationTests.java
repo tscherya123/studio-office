a3544f3b5742b9d0d04c6186bf0295233b6ebd90
tscherya123 <tscherya123@gmail.com>
2021-05-26 15:01:29 +0300

    Initial repository

---
+++ src/test/java/com/fetty/studiooffice/StudioOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fetty.studiooffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
